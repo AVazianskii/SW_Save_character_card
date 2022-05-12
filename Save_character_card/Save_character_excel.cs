d04622dad6029b806a6d59a00e4942e6d946f20e
AVazianskii <avazianskii@mail.ru>
2022-05-12 22:33:05 +0300

    Now this class is singletone

--- Save_character_card/Save_character_excel.cs
+++ Save_character_card/Save_character_excel.cs
@@ -6,8 +6,25 @@ namespace Character_design
 {
     public class Save_character_excel
     {
+        private static Save_character_excel Character_instance;
 
+
+
+        public static Save_character_excel GetInstance()
+        {
+            if (Character_instance == null)
+            {
+                Character_instance = new Save_character_excel();
+            }
+            return Character_instance;
+        }
         public void Save_character_to_Excel_card (Character character)
+        {
+
+        }
+
+
+        private Save_character_excel()
         {
 
         }
