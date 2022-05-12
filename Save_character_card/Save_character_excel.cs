d25bcbba929f7e3311d93e04500c0db049a1431c
AVazianskii <avazianskii@mail.ru>
2022-05-12 22:23:12 +0300

    Добавьте файлы проекта.

---
+++ Save_character_card/Save_character_excel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Character_design
+{
+    public class Save_character_excel
+    {
+
+        public void Save_character_to_Excel_card (Character character)
+        {
+
+        }
+    }
+}
