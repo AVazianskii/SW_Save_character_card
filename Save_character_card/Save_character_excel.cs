7216859b02a4f3502ea35e17b3b3bcc2b44f3fdf
AVazianskii <avazianskii@mail.ru>
2023-01-25 16:47:23 +0300

    Добавлена логика проверки файлов и создания директорий при их отсутствии

--- Save_character_card/Save_character_excel.cs
+++ Save_character_card/Save_character_excel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Windows;
+
 
 
 namespace Character_design
@@ -8,6 +11,12 @@ namespace Character_design
     public class Save_character_excel
     {
         private static Save_character_excel Character_instance;
+
+
+
+        private string player_cards_directory,
+                       player_card_template,
+                       character_directory;
 
 
 
@@ -19,16 +28,38 @@ namespace Character_design
             }
             return Character_instance;
         }
-        public void Save_character_to_Excel_card (Character character)
+        public void Save_character_to_Excel_card (out string error_msg)
         {
+            error_msg = "";
+            character_directory = player_cards_directory + $"\\{Character.GetInstance().Name}";
 
+
+            if (File.Exists(player_card_template))
+            {
+                // директория с карточками уже созданных персонажей
+                if (Directory.Exists(player_cards_directory) == false)
+                {
+                    Directory.CreateDirectory(player_cards_directory);
+                }
+                // директория с карточкой конкретного персонажа
+                if (Directory.Exists(character_directory) == false)
+                {
+                    Directory.CreateDirectory(character_directory);
+                }
+                File.Copy(player_card_template, character_directory + $"\\{Character.GetInstance().Name}" + ".xlsx");
+            }
+            else
+            {
+                error_msg = "Создание анкеты персонажа невозможно! Отсутствует шаблон анкеты.";
+            }
         }
 
 
 
         private Save_character_excel()
         {
-
+            player_cards_directory = Directory.GetCurrentDirectory() + "\\Player_cards";
+            player_card_template = Directory.GetCurrentDirectory() + "\\Player_card_template\\Template_v2.xlsx";
         }
     }
 }
