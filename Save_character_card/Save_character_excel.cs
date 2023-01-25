6b667b7aafe7ddb113e522731f8c9b9f11a56ffc
AVazianskii <avazianskii@mail.ru>
2023-01-25 21:51:42 +0300

    Добавлены некоторые функции записи в Excel при помощи пакета EPPlus

--- Save_character_card/Save_character_excel.cs
+++ Save_character_card/Save_character_excel.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.Office.Interop.Excel;
 using System.IO;
 using System.Windows;
+using OfficeOpenXml;
+using SW_Character_creation;
 
 
 
@@ -16,7 +17,8 @@ namespace Character_design
 
         private string player_cards_directory,
                        player_card_template,
-                       character_directory;
+                       character_directory,
+                       character_file;
 
 
 
@@ -32,7 +34,7 @@ namespace Character_design
         {
             error_msg = "";
             character_directory = player_cards_directory + $"\\{Character.GetInstance().Name}";
-
+            character_file = character_directory + $"\\{Character.GetInstance().Name}" + ".xlsx";
 
             if (File.Exists(player_card_template))
             {
@@ -46,7 +48,101 @@ namespace Character_design
                 {
                     Directory.CreateDirectory(character_directory);
                 }
-                File.Copy(player_card_template, character_directory + $"\\{Character.GetInstance().Name}" + ".xlsx");
+                File.Copy(player_card_template, character_file, true);
+
+                /*
+                Excel.Application exl_app = new Excel.Application();
+                Excel.Workbook exl_book = exl_app.Workbooks.Open(character_file);
+                Excel.Worksheet Character_card = exl_book.Sheets[1];
+
+                Character_card.Cells[2, 1].Value = Character.GetInstance().Name;
+
+                exl_book.Close(true);
+                exl_app.Quit(); */
+                //System.Runtime.InteropServices.Marshal.FinalReleaseComObject(exl_app);
+
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+                using (var package = new ExcelPackage(new FileInfo(character_file)))
+                {
+                    // Заполняем поле общей информации о персонаже
+                    ExcelWorksheet Character_card = package.Workbook.Worksheets[0];
+                    Character_card.Cells[2, 1].Value = Character.GetInstance().Name + ", " + Character.GetInstance().Sex;
+                    Character_card.Cells[3, 1].Value = Character.GetInstance().Character_race.Get_race_name();
+                    Character_card.Cells[4, 2].Value = Character.GetInstance().Age.ToString();
+                    Character_card.Cells[4, 4].Value = Character.GetInstance().Karma.ToString();
+                    Character_card.Cells[5, 2].Value = Character.GetInstance().Range.Get_range_name();
+                    Character_card.Cells[5, 4].Value = Character.GetInstance().Experience_left.ToString();
+
+                    // Заполняем поля атрибутов
+                    Character_card.Cells[09, 3].Value = Character.GetInstance().Strength.Get_atribute_score().ToString();
+                    Character_card.Cells[10, 3].Value = Character.GetInstance().Stamina.Get_atribute_score().ToString();
+                    Character_card.Cells[11, 3].Value = Character.GetInstance().Agility.Get_atribute_score().ToString();
+                    Character_card.Cells[12, 3].Value = Character.GetInstance().Quickness.Get_atribute_score().ToString();
+                    Character_card.Cells[13, 3].Value = Character.GetInstance().Intelligence.Get_atribute_score().ToString();
+                    Character_card.Cells[14, 3].Value = Character.GetInstance().Perception.Get_atribute_score().ToString();
+                    Character_card.Cells[15, 3].Value = Character.GetInstance().Charm.Get_atribute_score().ToString();
+                    Character_card.Cells[16, 3].Value = Character.GetInstance().Willpower.Get_atribute_score().ToString();
+
+                    // Заполняем поля боевых параметров
+                    foreach (Skill_Class skill in Character.GetInstance().Skills)
+                    {
+                        switch(skill.ID)
+                        {
+                            case 16: Character_card.Cells[09, 5].Value = skill.Score; break;
+                            case 17: Character_card.Cells[11, 5].Value = skill.Score; break;
+                            case 12: Character_card.Cells[13, 5].Value = skill.Score; break;
+                            case 13: Character_card.Cells[15, 5].Value = skill.Score; break;
+                            case 10: Character_card.Cells[17, 5].Value = skill.Score; break;
+                            case 09: Character_card.Cells[09, 7].Value = skill.Score; break;
+                            case 11: Character_card.Cells[11, 7].Value = skill.Score; break;
+                            case 07: Character_card.Cells[13, 7].Value = skill.Score; break;
+                            case 18: Character_card.Cells[15, 7].Value = skill.Score; break;
+                            case 15: Character_card.Cells[17, 7].Value = skill.Score; break;
+                        }
+                    }
+
+                    // Заполняем поля пирамиды ранений и штрафов за них
+                    Character_card.Cells[09, 08].Value = Character.GetInstance().Scratch_lvl.ToString();
+                    Character_card.Cells[19, 08].Value = Character.GetInstance().Scratch_penalty.ToString();
+                    Character_card.Cells[10, 09].Value = Character.GetInstance().Light_wound_lvl.ToString();
+                    Character_card.Cells[18, 09].Value = Character.GetInstance().Light_wound_penalty.ToString();
+                    Character_card.Cells[11, 10].Value = Character.GetInstance().Medium_wound_lvl.ToString();
+                    Character_card.Cells[17, 10].Value = Character.GetInstance().Medium_wound_penalty.ToString();
+                    Character_card.Cells[12, 11].Value = Character.GetInstance().Tough_wound_lvl.ToString();
+                    Character_card.Cells[16, 11].Value = Character.GetInstance().Tough_wound_penalty.ToString();
+                    Character_card.Cells[14, 12].Value = Character.GetInstance().Scratch_lvl.ToString();
+
+                    // Заполняем расчитанные боевые параметры
+                    Character_card.Cells[11, 14].Value = Character.GetInstance().Reaction.ToString();
+                    Character_card.Cells[13, 14].Value = Character.GetInstance().Armor.ToString();
+                    Character_card.Cells[15, 14].Value = Character.GetInstance().Force_resistance.ToString();
+                    Character_card.Cells[17, 14].Value = Character.GetInstance().Hideness.ToString();
+                    Character_card.Cells[19, 14].Value = Character.GetInstance().Watchfullness.ToString();
+                    Character_card.Cells[21, 14].Value = Character.GetInstance().Concentration.ToString();
+
+                    // Заполняем боевые формы и формы Силы
+                    byte i = 0;
+                    foreach (Abilities_sequence_template sequence in Character.GetInstance().Combat_sequences_with_points)
+                    {
+                        Character_card.Cells[36 + i, 08].Value = sequence.Name;
+                        Character_card.Cells[36 + i, 13].Value = sequence.Level;
+                        i = (byte)(i + 1);
+                    }
+                    if (Character.GetInstance().Forceuser)
+                    {
+                        foreach (Abilities_sequence_template sequence in Character.GetInstance().Force_sequences_with_points)
+                        {
+                            Character_card.Cells[36 + i, 08].Value = sequence.Name;
+                            Character_card.Cells[36 + i, 13].Value = sequence.Level;
+                            i = (byte)(i + 1);
+                        }
+                    }
+                    
+
+                    package.Save();
+                    //package.SaveAs();
+                }
             }
             else
             {
@@ -60,6 +156,13 @@ namespace Character_design
         {
             player_cards_directory = Directory.GetCurrentDirectory() + "\\Player_cards";
             player_card_template = Directory.GetCurrentDirectory() + "\\Player_card_template\\Template_v2.xlsx";
+        }
+
+
+
+        private void CheckExistCharacterCard()
+        {
+
         }
     }
 }
